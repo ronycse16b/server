6c33092333a3ea7bc12ec82d68d3f5b71ccda28e
ronycse16b <rony.cse16b@gmail.com>
2023-03-31 20:23:43 +0600

    hi

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const { query } = require('express');
-require('dotenv').config();
+
+
 
 
 // midelware.config
 app.use(cors());
 app.use(express.json());
+require('dotenv').config();
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qnzupqp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
